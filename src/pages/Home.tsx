731a9e2fcf5e625620203b1ce85086987ea6c4af
JosePott <joseignaciopott2002@gmail.com>
2024-10-04 21:25:11 -0300

    Añadido breadcrumb al header en Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,61 @@
-import { 
-  IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonTabBar, IonTabButton, IonIcon 
+import React, { useState } from 'react';
+import {
+  IonBreadcrumb,
+  IonBreadcrumbs,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonTab,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonThumbnail,
+  IonImg,
+  IonInput
 } from '@ionic/react';
 import { playCircle, radio, library, heart } from 'ionicons/icons';
+import './Home.css';
 
 const Home: React.FC = () => {
+  const [maxBreadcrumbs, setMaxBreadcrumbs] = useState<number | undefined>(4);
+
   return (
-    <IonPage>
+    <IonPage id="main-content">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tus Libros</IonTitle>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Tus libros</IonTitle>
         </IonToolbar>
+        {/* Breadcrumbs */}
+        <IonBreadcrumbs maxItems={maxBreadcrumbs} onIonCollapsedClick={() => setMaxBreadcrumbs(undefined)}>
+          <IonBreadcrumb href="#home">Home</IonBreadcrumb>
+          <IonBreadcrumb href="/login">Login</IonBreadcrumb>
+        </IonBreadcrumbs>
       </IonHeader>
+
       <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tus libros</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        {/* Tarjetas con imágenes, títulos y reseñas */}
         <IonCard>
           <img alt="Silhouette of mountains" src="public/libro1.jpeg" />
           <IonCardHeader>
@@ -19,7 +63,7 @@ const Home: React.FC = () => {
             <IonCardSubtitle>COOLLEN HOVER</IonCardSubtitle>
           </IonCardHeader>
           <IonCardContent>
-            Lily no siempre ha tenido una vida fácil, pero eso nunca le ha impedido luchar por lo que quiere y ha recorrido un largo camino para llegar a donde está ahora.
+            Lily no siempre ha tenido una vida fácil, pero eso nunca le ha impedido luchar por lo que quiere y ha recorrido un largo camino para llegar a donde está ahora. Su vida comienza a cambiar el día que Ryle Kincaid, un extraordinario neurocirujano, se fija en ella.
           </IonCardContent>
         </IonCard>
 
@@ -30,7 +74,7 @@ const Home: React.FC = () => {
             <IonCardSubtitle>J.K ROWLING</IonCardSubtitle>
           </IonCardHeader>
           <IonCardContent>
-            El día en que cumple once años, Harry Potter descubre que es hijo de dos conocidos hechiceros.
+            El día en que cumple once años, Harry Potter descubre que es hijo de dos conocidos hechiceros, de los que ha heredado poderes mágicos. Deberá acudir entonces a una famosa escuela de magia y hechicería: Howards.
           </IonCardContent>
         </IonCard>
 
@@ -41,12 +85,10 @@ const Home: React.FC = () => {
             <IonCardSubtitle>VERONICA ROTH</IonCardSubtitle>
           </IonCardHeader>
           <IonCardContent>
-            Divergente es una novela distópica donde la sociedad está dividida en cinco facciones según virtudes.
+            Divergente es una novela distópica donde la sociedad está dividida en cinco facciones según virtudes. La protagonista, Beatrice "Tris" Prior, descubre que es "divergente," alguien que no encaja en ninguna facción, lo que la pone en peligro. Mientras se une a la facción Osadía, debe enfrentarse a una conspiración que amenaza con destruir su mundo.
           </IonCardContent>
         </IonCard>
       </IonContent>
-
-
     </IonPage>
   );
 };
